539a26bf15078147993651ecc4593a7bdbd2ba72
Zi Chen <532764059@qq.com>
2024-04-28 00:43:26 +0100

    Camel Case Conversion Solution

--- Solutions/Class1.cs
+++ Solutions/Class1.cs
@@ -1,8 +1,25 @@
 ﻿namespace Solutions;
-
+using System;
+using System.Text;
 public class Class1
 {
     //Convert string to camel case
+    public static string ToCamelCase(string str)
+    {
+        StringBuilder ns = new StringBuilder("");
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == '-' || str[i] == '_')
+            {
+                ns.Append(char.ToUpper(str[i + 1]));
+                i += 1;
+            }
+            else
+            {
+                ns.Append(str[i]);
+            }
 
-
+        }
+        return ns.ToString();
+    }
 }
