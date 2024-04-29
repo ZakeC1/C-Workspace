02eee5784ecb95ec04ac14f2fc0ed5128036068f
Zi Chen <532764059@qq.com>
2024-04-29 15:58:03 +0100

    Summation of 3 or 5 multiples puzzle

--- Solutions/Class1.cs
+++ Solutions/Class1.cs
@@ -35,4 +35,8 @@ public class Class1
             return ("Odd");
         }
     }
+
+    //Summation of multiples of 3 or 5 under a given value. If the number is a multiple of both 3 and 5, only count it once.
+
+
 }
